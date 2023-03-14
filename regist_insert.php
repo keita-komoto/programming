3a3e810210fa9902b01673801e7783f607497d9e
keita-komoto <makibiso200x@yahoo.co.jp>
2023-03-14 19:12:57 +0900

    20230314 zantei

--- regist_insert.php
+++ regist_insert.php
@@ -5,6 +5,8 @@ function myExceptionHandler ($e) {
 }
 set_exception_handler('myExceptionHandler');
 
+
+
 if ( isset($_SESSION['family_name'])){
     $family_name = $_SESSION['family_name'];
     $last_name = $_SESSION['last_name'];
@@ -20,20 +22,23 @@ if ( isset($_SESSION['family_name'])){
     $authority = $_SESSION['authority'];
 }
 $pdo = new PDO(
-   'mysql:dbname=programming;host=localhost;charset=utf8mb4',
+    'mysql:dbname=programming;host=localhost;charset=utf8mb4',
     'root',
     '',
     );
     $hash = password_hash($_SESSION['password'], PASSWORD_DEFAULT);
+    $date = date('Y-m-d H:i:s');
     $pdo -> exec("INSERT INTO account(family_name,last_name,family_name_kana,last_name_kana,
         mail,`password`,gender,
         postal_code,prefecture,address_1,address_2,
-        authority)
+        authority,
+        delete_flag,registered_time)
     values(
         '".$_SESSION['family_name']."','".$_SESSION['last_name']."','".$_SESSION['family_name_kana']."','".$_SESSION['last_name_kana']."',
         '".$_SESSION['mail']."','.$hash.','".$_SESSION['gender']."',
         '".$_SESSION['postal_code']."','".$_SESSION['prefecture']."','".$_SESSION['address_1']."','".$_SESSION['address_2']."',
-        '".$_SESSION['authority']."');"
+        '".$_SESSION['authority']."',
+        '"."0"."','".$date."');"
     );
     header("Location:http://localhost/diworks/programming/regist_complete.php");
 ?>
