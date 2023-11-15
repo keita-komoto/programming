f4d60f0ab6d70c37c516c38ed8f4735a2a56e19b
keita-komoto <makibiso200x@yahoo.co.jp>
2023-11-15 22:00:57 +0900

    20231115

---
+++ java/enzan.java
@@ -0,0 +1,19 @@
+
+public class enzan {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int suugaku = 45;
+		int eigo = 82;
+		if (suugaku > 50 || eigo > 50 ) {
+			System.out.println("この人は合格です");
+		}
+		
+		String str1 = "鈴木";
+		System.out.println(str1);
+		char char1 ='A';
+		System.out.println(char1);
+		System.out.println(str1 + char1 + "さん");
+	}
+
+}
---
+++ java/hensu.java
@@ -0,0 +1,16 @@
+
+public class hensu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a1 = 5/2;
+		System.out.println(a1);
+
+		float a = (float)10/3;
+		System.out.println(a);
+		
+		int a2 = 6 % 4;
+		System.out.println(a2);
+	}
+
+}
