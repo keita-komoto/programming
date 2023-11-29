56d5565c2013077c37df7e7a168fd852f4c2cf4d
keita-komoto <makibiso200x@yahoo.co.jp>
2023-11-29 21:38:36 +0900

    20231129

---
+++ java/human/src/jp/co/diworks/action/Human.java
@@ -0,0 +1,21 @@
+package jp.co.diworks.action;
+
+public class Human {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		HumanName abc = new HumanName();
+		System.out.println(abc.getName());
+		
+		HumanAge efg = new HumanAge();
+		System.out.println(efg.getAge());
+		
+		HumanAddress hij = new HumanAddress();
+		System.out.println(hij.getAddress());
+		
+		HumanGender klm = new HumanGender();
+		System.out.println(klm.getGender());
+	}
+
+}
